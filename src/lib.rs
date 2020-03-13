0d0373de99c1d59314fab0de8322a8dc7bfbe9b6
Nate Finger <natefinger@Nates-MacBook-Pro.local>
2020-03-12 23:24:38 -0400

    update return value

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
 extern crate tokio;
-use reqwest::Error;
+use reqwest::{Error};
+use serde_json::{Value};
 
 const URL: &'static str = "http://esummarizer.com/main/getsummary";
 
 #[tokio::main]
-pub async fn summarize_text(text: &str) -> Result<String, Error> {
+pub async fn summarize_text(text: &str) -> Result<Value, Error> {
     let params = [
         ("text", text),
         ("nbsentences", "5")
     ];
     let client = reqwest::Client::new();
-    client.post(URL)
+    let res = client.post(URL)
         .form(&params)
         .send()
-        .await?
-        .text()
-        .await
+        .await?;
+    let text = res.text().await?;
+    let json_str: Value = serde_json::from_str(&text).unwrap();
+    Ok(json_str)
 }
