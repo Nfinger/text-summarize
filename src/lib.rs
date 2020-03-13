2231397eb458db29e3c990fab541e24f6bddc91e
Nate Finger <natefinger@Nates-MacBook-Pro.local>
2020-03-12 23:53:06 -0400

    use blocking

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,17 @@
-extern crate tokio;
-
+use std::error::Error;
+use reqwest::blocking::multipart;
 const URL: &'static str = "http://esummarizer.com/main/getsummary";
 
-#[tokio::main]
-pub async fn summarize_text(text: &str) -> String {
-    let params = [
-        ("text", text),
-        ("nbsentences", "5")
-    ];
-    let client = reqwest::Client::new();
-    client.post(URL)
-        .form(&params)
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap()
+pub fn summarize_text(text: &str) -> Result<String, Box<dyn Error>> {
+    let form = multipart::Form::new()
+        .text("text", text.to_string())
+        .text("nbsentences", "5");
+
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .post(URL)
+        .multipart(form)
+        .send()?;
+    let text = resp.text()?;
+    Ok(text)
 }
