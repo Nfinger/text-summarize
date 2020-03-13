1047bec1f237f4a1b1d3934833d99867eb224258
Nate Finger <natefinger@Nates-MacBook-Pro.local>
2020-03-12 23:41:50 -0400

    try sending just the string

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 extern crate tokio;
-use reqwest::{Error};
-use serde_json::{Value};
 
 const URL: &'static str = "http://esummarizer.com/main/getsummary";
 
 #[tokio::main]
-pub async fn summarize_text(text: &str) -> Result<Value, Error> {
+pub async fn summarize_text(text: &str) -> String {
     let params = [
         ("text", text),
         ("nbsentences", "5")
     ];
     let client = reqwest::Client::new();
-    let res = client.post(URL)
+    client.post(URL)
         .form(&params)
         .send()
-        .await?;
-    let text = res.text().await?;
-    let json_str: Value = serde_json::from_str(&text).unwrap();
-    Ok(json_str)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
 }
